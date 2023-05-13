5e5ce2132bea71e04adbc50328fb9a984ede5ddc
Levy <kipkoechlevy096@gmail.com>
2023-05-13 21:24:04 +0300

    corrected errors

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = ()=>{
         <div id="projects" className="w-full">
             <div className="mx-w-[1240px] mx-auto px-2 py-16">
                 <p className="text-xl tracking-widest uppercase text-[#5651e5]">Projects</p>
-                <h2 className="py-4">What I've Built</h2>
+                <h2 className="py-4">What I&apos;ve Built</h2>
                 <div className="grid md:grid-cols-2 gap-8 ">
                    <ProjectTitle title="children's home" backgroundImg={childrenImg} projectUrl="https://childrens-home-app.vercel.app/login"/>
                    <ProjectTitle title="online shop" backgroundImg={shopImg} projectUrl="https://phase-4-project-ten.vercel.app/signup"/>
--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -9,7 +9,7 @@ const Main = () =>{
         <div className="max-w-[1240px w-full h-full mx-auto p-2 flex justify-center items-center">
             <div>
                 <p className="uppercase text-sm tracking-widest text-gray-600">LET'S BUILD SOMETHING</p>
-                <h1 className="py-4 text-gray-700">Hey I'm <span className="text-[#5651e5]">Levy</span></h1>
+                <h1 className="py-4 text-gray-700">Hey I&apos;m <span className="text-[#5651e5]">Levy</span></h1>
                 <h1 className="py-2 text-gray-700">A software developer</h1>
                 <p className="py-4 text-gray-600 max-w-[70%] m-auto">
                     As a recent software developer graduate with passion for software development, I am
