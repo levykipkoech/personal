4a64701f9903b21c4cd3b8211b1c50b30146aa04
Levy <kipkoechlevy096@gmail.com>
2023-05-16 16:46:58 +0300

    added fullstop

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -7,13 +7,13 @@ import {AiOutlineMail} from "react-icons/ai"
 const Main = () =>{
     return(
        <div id="main" className="w-full h-screen text-center">
-        <div className="max-w-[1240px w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
             <div>
                 <p className="uppercase text-sm tracking-widest text-gray-600">LET&apos;S BUILD SOMETHING</p>
                 <h1 className="py-4 text-gray-700">Hey I&apos;m <span className="text-[#5651e5]">Levy</span></h1>
                 <h1 className="py-2 text-gray-700">A software developer</h1>
                 <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-                    As a recent software developer graduate with passion for software development, I am
+                    I am recent software developer graduate with passion for software development. I am
                     seeking a software developer position that will allow me to apply my skills and knowledge
                     to contribute to the development of cutting edge software solutions. I am a quick learner,
                     detail-oriented, and committed to delivering high-quality code.
